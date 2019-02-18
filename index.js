f15bd1a8f678d7e1b9ab71a9a7a27e2086b18a0d
jane-nic <45681194+jane-nic@users.noreply.github.com>
2019-02-18 10:30:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions');
+const admin =require('firebase-admin');
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall(function(data,context){
+    //check request is made by an admin
+    if(context.auth.token.admin!== true){
+        return {error: 'only admin can add admin suckker!!'}
+    }
+    //get user and add custom claim
+    return admin.auth().getUserByEmail(data.email).then(function(user){
+        return admin.auth().setCustomUserClaims(user.uid,{
+           admin:true 
+        });
+    }).then(function(){
+        return {
+            message:'success! $(data.email) has been made an admin'
+        }
+    });
+
+})
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
